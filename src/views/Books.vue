ee4bb701908c60ac101a95a2026c747662c4321e
BK Chen <k97895732@gmail.com>
2021-03-09 22:10:03 +0800

    建立基本架構

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div class="page-wrap">
+    <div class="book-list-wrapper">
+      <div class="book-card-wrap">
+        <div class="book-card"></div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
